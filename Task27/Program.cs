bf5be2f367d7af778adfe552bc78ef3afa83942f
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-22 20:46:43 +0300

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+    sum += number % 10;
+    number /= 10;
+}
+Console.WriteLine(sum);
+
