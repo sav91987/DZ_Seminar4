e90f783ae88472d655910e8e04b6ff44d40d7e48
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-22 20:48:37 +0300

    Added Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число элементов массива: ");
+int length = int.Parse(Console.ReadLine());
+
+int[] GetArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(11);
+    }
+    return array;
+}
+
+int[] resultArray = GetArray(length);
+
+Console.WriteLine($"[{String.Join("\t", resultArray)}]");
