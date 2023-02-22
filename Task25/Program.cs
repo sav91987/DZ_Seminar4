791ea7fdcbe84fdb4b086235e89ddfac7cca74a1
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-22 20:44:23 +0300

    Added Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+ int result = 1;
+for (int i = 1; i <= B; i++)
+{
+result *= A;
+}
+Console.WriteLine(result);
+
